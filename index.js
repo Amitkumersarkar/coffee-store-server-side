42feda5dfa503adeed7cd7f8d4ac6ac1b8e0aec6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-19 22:08:48 +0600

    added updated and delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5500;
 
@@ -38,6 +38,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        // update method
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
         // connection client side data into server side by post operation
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
@@ -46,6 +53,15 @@ async function run() {
             res.send(result);
         })
 
+        // delete operations
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
